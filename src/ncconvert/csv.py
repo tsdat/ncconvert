794859713d33067f58b5a7b0574b4ad623a834d3
Maxwell Levin <24307537+maxwelllevin@users.noreply.github.com>
2023-04-25 16:33:59 -0700

    Add to_csv_collection

--- src/ncconvert/csv.py
+++ src/ncconvert/csv.py
@@ -1,11 +1,11 @@
 from __future__ import annotations
 
-import xarray as xr
-from typing import Dict, Any
-
-
 import json
+from collections import defaultdict
 from pathlib import Path
+from typing import Any, Dict
+
+import xarray as xr
 
 
 # Uses vanilla xarray and pandas settings to convert xarray to csv
@@ -24,8 +24,8 @@ def to_vanilla_csv(
 
     df = dataset.to_dataframe(dim_order=list(dataset.dims))
     df.to_csv(filepath, **to_csv_kwargs)  # type: ignore
-    metadata = dataset.to_dict(data=False, encoding=True)
 
+    metadata = dataset.to_dict(data=False, encoding=True)
     metadata_json = json.dumps(metadata, indent=4)
     metadata_path = Path(filepath).with_suffix(".json")
     metadata_path.write_text(metadata_json)
@@ -33,76 +33,41 @@ def to_vanilla_csv(
     return Path(filepath), metadata_path
 
 
-# class CSVWriter:
-#     """---------------------------------------------------------------------------------
-#     Converts a `xr.Dataset` object to a pandas `DataFrame` and saves the result to a csv
-#     file using `pd.DataFrame.to_csv()`. Properties under the `to_csv_kwargs` parameter
-#     are passed to `pd.DataFrame.to_csv()` as keyword arguments.
+# Creates a collection of csv files. Csv files are split such that each file contains a
+# unique index structure. For datasets with only 1 dimension this is exactly the same as
+# the `to_vanilla_csv()` method. For a dataset with dimensions time & height with some
+# variables dimensioned by time, some by height, and some dimensioned by both, this will
+# result in 3 csv files; one for variables dimensioned only by time, another for
+# variables dimensioned only by height, and one more for variables dimensioned by both.
+# CSV files with more than one dimension will be indexed by the cartesian product of the
+# dimensions (same as `to_vanilla_csv()`). Metadata is stored in a separate file.
+def to_csv_collection(
+    dataset: xr.Dataset,
+    filepath: str | Path,
+    to_csv_kwargs: Dict[str, Any] | None = None,
+) -> tuple[tuple[Path, ...], Path]:
+    if to_csv_kwargs is None:
+        to_csv_kwargs = {}
 
-#     ---------------------------------------------------------------------------------"""
+    Path(filepath).parent.mkdir(parents=True, exist_ok=True)
 
-#     class Parameters:
-#         dim_order: Optional[List[str]] = None
-#         to_csv_kwargs: Dict[str, Any] = {}
+    # Get variable dimension groupings
+    dimension_groups: dict[tuple[str, ...], list[str]] = defaultdict(list)
+    for var_name, data_var in dataset.data_vars.items():
+        dims = tuple(str(d) for d in data_var.dims)
+        dimension_groups[dims].append(var_name)
 
-#     parameters: Parameters = Parameters()
-#     file_extension: str = ".csv"
+    # Save each dimension group to a file
+    csv_filepaths: list[Path] = []
+    for dim_group, variable_names in dimension_groups.items():
+        df = dataset[variable_names].to_dataframe(dim_order=dim_group)
+        dim_group_path = Path(filepath).with_suffix(f".{'.'.join(dim_group)}.csv")
+        csv_filepaths.append(dim_group_path)
+        df.to_csv(dim_group_path, **to_csv_kwargs)
 
-#     def write(
-#         self, dataset: xr.Dataset, filepath: Optional[Path] = None, **kwargs: Any
-#     ) -> None:
-#         # QUESTION: Is this format capable of "round-tripping"?
-#         # (i.e., ds != read(write(ds)) for csv format)
-#         d1: List[Hashable] = []
-#         d2: List[Hashable] = []
-#         d2_coord: List[Hashable] = [v for v in dataset.coords if v != "time"]
-#         for var in dataset:
-#             shp = dataset[var].shape
-#             if len(shp) <= 1:
-#                 d1.append(var)
-#             elif len(shp) == 2:
-#                 d2.append(var)
-#             else:
-#                 warnings.warn(
-#                     "CSV writer cannot save variables with more than 2 dimensions."
-#                 )
+    metadata = dataset.to_dict(data=False, encoding=True)
+    metadata_json = json.dumps(metadata, indent=4)
+    metadata_path = Path(filepath).with_suffix(".json")
+    metadata_path.write_text(metadata_json)
 
-#         # Save header data
-#         header_filepath = filepath.with_suffix(".hdr.csv")  # type: ignore
-#         header = dataset.attrs
-#         with open(str(header_filepath), "w", newline="\n") as fp:
-#             for key in header:
-#                 fp.write(f"{key},{header[key]}\n")
-
-#         # Save variable metadata
-#         metadata_filepath = filepath.with_suffix(".attrs.csv")  # type: ignore
-#         var_metadata: List[Dict[str, Any]] = []
-#         for var in dataset:
-#             attrs = dataset[var].attrs
-#             attrs.update({"name": var})
-#             var_metadata.append(attrs)
-#         df_metadata = pd.DataFrame(var_metadata)
-#         df_metadata = df_metadata.set_index("name")  # type: ignore
-#         df_metadata.to_csv(metadata_filepath)
-
-#         if d1:
-#             # Save 1D variables
-#             dim1_filepath = filepath.with_suffix(".time.1d.csv")  # type: ignore
-#             ds_1d = dataset.drop_vars(d2)  # drop 2D variables
-#             ds_1d = ds_1d.drop_vars(d2_coord)
-#             df_1d = ds_1d.to_dataframe()
-#             df_1d.to_csv(dim1_filepath, **self.parameters.to_csv_kwargs)  # type: ignore
-
-#         if d2:
-#             # Save 2D variables
-#             for coord in d2_coord:
-#                 dim2_filepath = filepath.with_suffix("." + coord + ".2d.csv")  # type: ignore
-#                 ds_2d = dataset.drop_vars(d1)  # drop 1D variables
-#                 other_dim_vars = [
-#                     v for v in ds_2d.data_vars if coord not in ds_2d[v].dims
-#                 ]
-#                 other_coords = d2_coord.copy()
-#                 other_coords.remove(coord)
-#                 ds_2d = ds_2d.drop_vars(other_dim_vars + other_coords)
-#                 df_2d = ds_2d.to_dataframe(self.parameters.dim_order)  # type: ignore
-#                 df_2d.to_csv(dim2_filepath, **self.parameters.to_csv_kwargs)  # type: ignore
+    return tuple(csv_filepaths), metadata_path
--- test/test_csv.py
+++ test/test_csv.py
@@ -1,9 +1,10 @@
-import pandas as pd
-import xarray as xr
+import json
+import os
 from pathlib import Path
 
-import json
+import pandas as pd
 import pytest
+import xarray as xr
 
 
 @pytest.fixture
@@ -12,7 +13,12 @@ def dataset() -> xr.Dataset:
         coords={
             "time": (
                 "time",
-                pd.date_range("2022-04-05", "2022-04-06", periods=3 + 1, inclusive="left"),  # type: ignore
+                pd.date_range(
+                    "2022-04-05",
+                    "2022-04-06",
+                    periods=3 + 1,
+                    inclusive="left",
+                ),  # type: ignore
                 {"units": "Seconds since 1970-01-01 00:00:00"},
             ),
             "height": (
@@ -54,7 +60,7 @@ def dataset() -> xr.Dataset:
 
 
 def test_vanilla_csv(dataset: xr.Dataset):
-    from src.ncconvert.csv import to_vanilla_csv
+    from ncconvert.csv import to_vanilla_csv
 
     filepath = Path(".tmp/data/vanilla.csv")
 
@@ -72,3 +78,40 @@ def test_vanilla_csv(dataset: xr.Dataset):
 
     assert "datastream" in meta["attrs"]
     assert "time" in meta["coords"]
+
+    os.remove(output_path)
+    os.remove(metadata_path)
+
+
+def test_csv_collection(dataset: xr.Dataset):
+    from ncconvert.csv import to_csv_collection
+
+    filepath = Path(".tmp/data/collection.20220405.000000.csv")
+
+    output_paths, metadata_path = to_csv_collection(dataset, filepath)
+
+    assert len(output_paths) == 3
+    assert filepath.with_suffix(".height.csv") in output_paths
+    assert filepath.with_suffix(".time.csv") in output_paths
+    assert filepath.with_suffix(".time.height.csv") in output_paths
+    assert metadata_path == filepath.with_suffix(".json")
+
+    h_df = pd.read_csv(sorted(output_paths)[0])  # type: ignore
+    assert len(h_df.index) == len(dataset.height)
+    assert list(h_df.columns) == ["height", "other"]
+
+    t_df = pd.read_csv(sorted(output_paths)[1])  # type: ignore
+    assert len(t_df.index) == len(dataset.time)
+    assert list(t_df.columns) == ["time", "humidity"]
+
+    th_df = pd.read_csv(sorted(output_paths)[2])  # type: ignore
+    assert len(th_df.index) == len(dataset.time) * len(dataset.height)
+    assert list(th_df.columns) == ["time", "height", "temperature"]
+
+    meta = json.loads(metadata_path.read_text())
+    assert "datastream" in meta["attrs"]
+    assert "time" in meta["coords"]
+
+    for output_path in output_paths:
+        os.remove(output_path)
+    os.remove(metadata_path)
